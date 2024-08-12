ef8980078d91bf10c904845964b43fc3f29cf28c
Na Young <70385710+nayeo2@users.noreply.github.com>
2024-08-12 10:25:14 +0900

    Update log_move.sh

--- log_move.sh
+++ log_move.sh
@@ -10,10 +10,10 @@ target_dir="$work_dir/$work_target_ymd"
 merged_log_file="$target_dir/${work_target_ymd}_merged.log"
 
 # 작업 폴더가 존재하지 않으면 생성
-sudo mkdir -p "$target_dir"
+mkdir -p "$target_dir"
 
 # 로그 파일을 백업 장소로 복사
-sudo cp "$bk_dir"/logcontroller."$work_target_ymd"_* "$target_dir"/
+cp "$bk_dir"/logcontroller."$work_target_ymd"_* "$target_dir"/
 
 # 병합할 파일들을 병합하여 하나의 파일로 저장
 cat "$target_dir"/logcontroller."$work_target_ymd"_* > "$merged_log_file"
